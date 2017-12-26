7acd41731490080636665028b7417a966fe4bddc
Matthew Mora <moramatt1@gmail.com>
2017-12-25 23:14:56 -0600

    made inputs work

--- main.java
+++ main.java
@@ -1,16 +1,16 @@
-public class Quadratic{
+import java.util.Scanner;
+
+class Quadratic{
 	public static void main(String[] args){
-		double a;
-		double b;
-		double c;
 		//get inputs
+		Scanner input = new Scanner(System.in);
 		System.out.println("ax^2 + bx + c");
 		System.out.print("a: ");
-		Scanner a = new Scanner(System.in);
+		double a = input.nextDouble();
 		System.out.print("b: ");
-		Scanner b = new Scanner(System.in);
-		System.out.print("c: ");
-		Scanner c = new Scanner(System.in);
+		double b = input.nextDouble();
+		System.out.print("c: ");		
+		double c = input.nextDouble();
 		//solve
 		System.out.print(a);
 		System.out.print("x^2 + (");
@@ -18,5 +18,6 @@ public class Quadratic{
 		System.out.print(")x + (");
 		System.out.print(c);
 		System.out.println(")");
+		System.out.println("Roots: ");
 	}
 }
