47226a0e05bdbb8c48dd72a300ba5089c78b23ff
Matthew Mora <moramatt1@gmail.com>
2017-12-25 22:53:26 -0600

    initial commit

---
+++ main.java
@@ -0,0 +1,22 @@
+public class Quadratic{
+	public static void main(String[] args){
+		double a;
+		double b;
+		double c;
+		//get inputs
+		System.out.println("ax^2 + bx + c");
+		System.out.print("a: ");
+		Scanner a = new Scanner(System.in);
+		System.out.print("b: ");
+		Scanner b = new Scanner(System.in);
+		System.out.print("c: ");
+		Scanner c = new Scanner(System.in);
+		//solve
+		System.out.print(a);
+		System.out.print("x^2 + (");
+		System.out.print(b);
+		System.out.print(")x + (");
+		System.out.print(c);
+		System.out.println(")");
+	}
+}
